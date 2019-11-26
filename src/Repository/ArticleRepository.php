ac7ade812268b4401a19c6e942597a75e410c62f
Jérôme <buteljerome@yahoo.fr>
2019-11-26 02:23:17 +0100

    Use ManagerRegistry instead of RegistryInterface in repositories

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -6,7 +6,7 @@ use App\Entity\Article;
 use App\Entity\User;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\ORM\QueryBuilder;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method Article|null find($id, $lockMode = null, $lockVersion = null)
@@ -16,7 +16,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class ArticleRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Article::class);
     }
